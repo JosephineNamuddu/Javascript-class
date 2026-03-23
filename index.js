ccf9f502e92c8ea896a953c1067deb2c047703fe
JosephineNamuddu <jorselyne@gmail.com>
2026-03-23 12:30:29 +0300

    first js class

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("My name is Josephine");
+// Variables
+// Declaring variables uses the var, let, or const keywords
+
+// var is function-scoped and can be redeclared and updated
+var name = "Josephine";
+console.log(name);
+name = "Namuddu Josephine";
+console.log(name);
+// let is block-scoped and can be updated but not redeclared
+let age = 35;
+console .log(age);
+age = 22;
+console.log("My age has changed becouse I am just a girl",age);
+
+
+
+
+
+
+
+
+// Data Types
+// Javascript has several datatypes including:
+// 1. String 
+let greeting = "Hello,How are you";
+console.log (greeting);
+console.log(typeof greeting);
+
+// 2. Number
+let x = 10;
+let y = "12"
+console.log(x);
+console .log (y);
+console.log(typeof x);
+console.log (typeof y);
